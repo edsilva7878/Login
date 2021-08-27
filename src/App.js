5c55533f381a67ac4745473530479011ce4c3949
edsilva7878 <edsilva7878@gmail.com>
2021-08-27 15:47:50 -0300

    SUBINDO HOME PAGE

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Web from "./Components/home/Web"
-import Direito from './Components/ailson/Direito'
 
 export default function App() {
   return (
 
     <div className="App">
       <Web/>
-      <Direito/>
     </div>
   );
 }
